8a7817dc463104ccfeb983f22d9e8c47df4e4ace
Eric Ihli <ericihli@gmail.com>
2016-10-31 21:28:13 -0700

    Start on run_job method
    This will queue up WorkerManagers and start the process. Should be run on cron.

--- lib/job_manager.rb
+++ lib/job_manager.rb
@@ -2,8 +2,36 @@ require 'securerandom'
 require_relative './persistence_manager'
 
 class JobManager
+  attr_accessor :pm
+
   def initialize
     @pm = PersistenceManager.instance
+  end
+
+  def run_jobs
+    cursor = @pm.db.query(<<-SQL)
+      select `workers`.*, `worker_managers`.* from worker_managers
+      join workers on workers.worker_manager_id = worker_managers.id
+    SQL
+
+    # Some ugly as sin console table formatting
+    # Cool. It works. Now get it out of here.
+    cursors_and_columns = cursor.columns.zip cursor.first
+    widths = cursors_and_columns.map { |c|
+      c.max { |a, b| a.to_s.length <=> b.to_s.length }.length
+    }
+
+    puts widths.join ", "
+    format = ""
+    widths.each_with_index { |w, i|
+      format = format + "%#{i+1}$#{w}s "
+    }
+    puts sprintf(format, *cursor.columns)
+    cursor.each { |row|
+      puts sprintf(format, *row.to_a)
+    }
+    cursor.close
+    nil
   end
 
   def create_jobs(worker_definitions)
