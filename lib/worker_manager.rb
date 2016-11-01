32b2db0584f9b166a9d2a296ceadc4f661e69cd2
Eric Ihli <ericihli@gmail.com>
2016-11-01 08:36:58 -0700

    Add pseudonotes to WorkerManager

--- lib/worker_manager.rb
+++ lib/worker_manager.rb
@@ -11,9 +11,37 @@ class WorkerManager
       select * from workers where workers.worker_manager_id = #{@id}
     SQL
     workers.each do |worker|
-      # Enqueue worker
-      # Wait...
       puts worker.join " "
+
+      # Check cache using workers 'state_key' to get its state
+      state = @pm.get(worker[1])
+
+      case state
+      when "pending"
+        # enqueue worker
+        # start polling procedure
+      when "started"
+        # if worker is already enqueued/running
+        #   start polling procedure
+        # else if worker is not enqueued/running
+        #   enqueue worker
+        #   start polling procedure
+      when "failed/success"
+        # handle completed states
+      else
+        # handle unknown state
+      end
+      puts state
     end
+  end
+
+  def poll(state_key)
+    # get state
+    # if state success
+    #   handle success
+    # if state failed
+    #   handle failed
+    # else
+    #   handle timeouts/retries/etc...
   end
 end
