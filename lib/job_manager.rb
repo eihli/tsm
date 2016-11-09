9672d9de1f34d779e1808632c847b940fb4b5132
Eric Ihli <ericihli@gmail.com>
2016-11-08 22:33:52 -0800

    Add comments to job manager

--- lib/job_manager.rb
+++ lib/job_manager.rb
@@ -8,22 +8,30 @@ class JobManager
     @pm = PersistenceManager.instance
   end
 
+  # Currently doesn't actualy run jobs.
+  # Spent too much time learning sprintf formatting
+  # to bother with that part of the method.
   def run_jobs
+    # TODO: ORM? DB Adapter?
     cursor = @pm.db.query(<<-SQL)
       select `workers`.*, `worker_managers`.* from worker_managers
       join workers on workers.worker_manager_id = worker_managers.id
     SQL
 
-    # Some ugly as sin console table formatting
+    # Some ugly as sin console table formatting follows
     # Cool. It works. Now get it out of here.
+    #
+    # Zip column headers with first row
     cursors_and_columns = cursor.columns.zip cursor.first
     widths = cursors_and_columns.map { |c|
+      # spaceship operator: https://en.wikipedia.org/wiki/Three-way_comparison
+      # Get the length(width) of the longer(wider) of the two columns
       c.max { |a, b| a.to_s.length <=> b.to_s.length }.length
     }
 
-    puts widths.join ", "
     format = ""
     widths.each_with_index { |w, i|
+      # "%<arg index>$<width/padding>s"
       format = format + "%#{i+1}$#{w}s "
     }
     puts sprintf(format, *cursor.columns)
@@ -45,7 +53,7 @@ class JobManager
 
     worker_definitions.each do |worker_definition|
       worker_uuid = SecureRandom.uuid
-      # TODO: Pull these queries out into PersistanceManager
+      # TODO: Pull these queries out
       @pm.db.execute <<-SQL
         insert into workers(uuid, state, worker_manager_id)
         values(\"#{worker_uuid}\", "pending", #{worker_manager_id})
